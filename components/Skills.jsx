0c1c73926be8024f5f9e2b6021eeb59b736b2416
amit <amit256375@gmail.com>
2023-11-10 10:53:27 +0530

    button

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <div className="max-w-[1280px]  mx-4 xl:mx-auto mt-10 md:mt-14">Skills</div>
+    <div className="max-w-[1280px]  mx-4 xl:mx-auto mt-10 md:mt-14">
+      <div className="flex mt-10 justify-center">
+        <h1 className="text-white font-bold text-3xl">Skills</h1>
+      </div>
+    </div>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import About from '@/components/About'
 import Navbar from '@/components/Navbar'
+import Skills from '@/components/Skills'
 import React from 'react'
 
 const index = () => {
@@ -7,6 +8,7 @@ const index = () => {
     <div className='bg-slate-700  '>
     <Navbar/>
     <About/>
+    <Skills/>
     </div>
 
   )
