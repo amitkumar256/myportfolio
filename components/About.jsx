78f0e4ae1e8c622565786ad2b403165d1ddfa41f
amit <amit256375@gmail.com>
2024-01-25 11:52:12 +0530

    added animation

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
 import React from "react";
+import { motion } from "framer-motion";
+
+// import { useSpring, animated } from "react-spring";
 
 const About = () => {
+  // const fadeIn = useSpring({
+  //   opacity: 1,
+  //   from: { opacity: 0 },
+  //   config: { duration: 3000 }, // Adjust the duration as needed
+  // });
+
   return (
     <div
       className="bg-cover bg-center "
@@ -24,19 +33,29 @@ const About = () => {
         </div>
         <div className="pt-10  flex flex-col-reverse sm:flex-row sm:justify-between items-center   ">
           <div className="sm:w-3/5">
-            <p className="text-xl font-serif sm:text-2xl text-yellow-100  leading-8 sm:leading-10 sm:tracking-wide pt-4 sm:pt-0">
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.2 }}
+              className="text-xl font-serif sm:text-2xl text-yellow-100  leading-8 sm:leading-10 sm:tracking-wide pt-4 sm:pt-0"
+            >
               Hi my name is{" "}
               <span className="text-3xl sm:text-4xl">Amit Kumar</span> , A
               passionate Front-End Developer.I specialize in bringing digital
               ideas to life through elegant and user-friendly interfaces. With a
               strong foundation in HTML, CSS, and the power of Next.js.
-            </p>
-            <p className="text-xl font-serif sm:text-2xl text-yellow-100  leading-8 sm:leading-10 sm:tracking-wide pt-4 ">
+            </motion.p>
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.2 }}
+              className="text-xl font-serif sm:text-2xl text-yellow-100  leading-8 sm:leading-10 sm:tracking-wide pt-4 "
+            >
               My love for clean and efficient code is matched only by my
               dedication to crafting visually appealing websites. Whether
               it&apos;s designing an intuitive user interface or diving into the
               intricacies of frontend development.
-            </p>
+            </motion.p>
           </div>
           <div className=" bg-gradient-to-r from-orange-400 to-red-600 hover:scale-105 transition-transform duration-500">
             <Image
